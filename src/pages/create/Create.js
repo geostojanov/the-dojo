19b0b383783641f3831f13d7536fb61a0b9e4f38
geostojanov <gestojanov@gmail.com>
2025-02-05 15:22:26 +0100

    assigning users to projects

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Select from 'react-select'
+import { useCollection } from '../../hooks/useCollection'
 
 // styles
 import './Create.css'
@@ -12,6 +13,9 @@ const categories = [
 ]
 
 export default function Create() {
+  const { documents } = useCollection('users')
+  const [users, setUsers ] = useState([])
+  
   // form fields
   const [name, setName] = useState('')
   const [details, setDetails] = useState('')
@@ -19,9 +23,18 @@ export default function Create() {
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
   
+  useEffect(() => {
+    if(documents) {
+      const options = documents.map(user => {
+        return { value: user, label: user.displayName }
+      })
+      setUsers(options)
+    }
+  }, [documents])
+  
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(name, details, dueDate, category.value)
+    console.log(name, details, dueDate, category.value, assignedUsers)
   }
   
   return (
@@ -63,7 +76,11 @@ export default function Create() {
         </label>
         <label>
           <span>Assign to:</span>
-          {/* select here later */}
+          <Select
+            onChange={(option) => setAssignedUsers(option)} 
+            options={users}
+            isMulti
+          />
         </label>
 
         <button className="btn">Add Project</button>
