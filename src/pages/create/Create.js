c9246c11c800ed300f6cae40ecd8b5429e93f526
geostojanov <gestojanov@gmail.com>
2025-02-05 15:39:14 +0100

    making a new project

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import Select from 'react-select'
 import { useCollection } from '../../hooks/useCollection'
+import { timestamp } from '../../firebase/config'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 // styles
 import './Create.css'
@@ -15,6 +17,7 @@ const categories = [
 export default function Create() {
   const { documents } = useCollection('users')
   const [users, setUsers ] = useState([])
+  const { user } = useAuthContext()
   
   // form fields
   const [name, setName] = useState('')
@@ -47,8 +50,30 @@ export default function Create() {
       return
     }
     
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid
+    }
     
-    console.log(name, details, dueDate, category.value, assignedUsers)
+    const assignedUsersList = assignedUsers.map(user => {
+      return {
+        displayName: user.value.displayName,
+        photoURL: user.value.photoURL,
+        id: user.value.id
+      }
+    })
+    
+    const project = {
+      name,
+      details,
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+      createdBy,
+      assignedUsersList
+    }
+    console.log(project)
   }
   
   return (
