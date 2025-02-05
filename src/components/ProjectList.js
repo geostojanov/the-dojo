c223e2abc224e55e925cd98506c59270feb16ad4
geostojanov <gestojanov@gmail.com>
2025-02-05 15:56:11 +0100

    fetching projects

---
+++ src/components/ProjectList.js
@@ -0,0 +1,15 @@
+
+
+// styles
+import './ProjectList.css';
+
+export default function ProjectList({ projects }) {
+  return (
+    <div>
+      {projects.length === 0 && <p>No projects yet!</p>}
+      {projects.map(project => (
+        <div key={project.id}>{project.name}</div>
+      ))}
+    </div>
+  )
+}
